ac0c018458258e9cbc31906f9df4aceac42a858e
Sandra <sandra.alpha2010@gmail.com>
2020-04-18 10:13:09 -0700

    shortened innerhtml object, added comments, added spaces for ease of reading

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,33 @@
-/*let userGuessesE1 = document.getElementById("user-guesses");
+
+/*   */
+let userGuessesE1 = document.getElementById("user-guesses");
 let guessesLeftE1 = document.getElementById("guesses-left");
 let userWinsE1 = document.getElementById("user-wins");
-let userLossesE1 = document.getElementById("user-losses");*/
+let userLossesE1 = document.getElementById("user-losses");
 
-
+/*   */
 let computerChoices = ["q", "z", "a", "e", "d", "c", "t", "g", "b", "u", "j", "m", "o", "l"];
 
+
+/*   */
 let guessesMax = 6;
 let displayPredictions = []
 
-
+/*   */
 let wins = 0;
 let losses = 0;
 
-
+/*  */
 document.onkeyup = function(event) {
 
+/*   */    
 let userPredictions = event.key;
 
+
+/*   */
 let computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+/*   */
 
 if ((userPredictions === "q" || (userPredictions === "z") || (userPredictions === "a") || 
 (userPredictions === "e") || (userPredictions === "d") || (userPredictions === "c") || 
@@ -27,28 +35,47 @@ if ((userPredictions === "q" || (userPredictions === "z") || (userPredictions ==
 (userPredictions === "u") || (userPredictions === "j") || (userPredictions === "m") || 
 (userPredictions === "o") || (userPredictions === "l")) ){
 
+
+    /*   */
         wins++;
-        document.getElementById("user-wins").innerHTML = `Wins: ${wins}`;
+
+        userWinsE1.innerHTML = `Wins: ${wins}`;
+
         guessesMax = 6;
-        document.getElementById("guesses-left").innerHTML = `Guesses left: ${guessesMax}`;
+
+        guessesLeftE1.innerHTML = `Guesses left: ${guessesMax}`;
+
         //displayPrediction = []; ?   
-        document.getElementById("user-guesses").innerHTML = `Guesses so far: `;
+        userGuessesE1.innerHTML = `Guesses so far: `;
+
         //displayPredictions resets
         guessesMax = 6;
-    } else {
-        displayPredictions.push(userPredictions);
-        document.getElementById("user-guesses").innerHTML = `Guesses so far: ${displayPredictions}`;
-        guessesMax--;
-        document.getElementById("guesses-left").innerHTML = `Guesses left: ${guessesMax}`;
-        
     } 
     
-    if (guessesMax === 0){
+    /*   */
+    else {
+        displayPredictions.push(userPredictions);
+
+        userGuessesE1.innerHTML = `Guesses so far: ${displayPredictions}`;
+
+        guessesMax--;
+
+        guessesLeftE1.innerHTML = `Guesses left: ${guessesMax}`;
+        
+    } 
+
+    /*  */
+        if (guessesMax === 0){
+
             losses++;
-            document.getElementById("user-losses").innerHTML = `Losses: ${losses}`;
-            document.getElementById("user-guesses").innerHTML = `Guesses so far: `;
+
+            userLossesE1.innerHTML = `Losses: ${losses}`;
+
+            userGuessesE1.innerHTML = `Guesses so far: `;
+
             guessesMax = 6;
-            document.getElementById("guesses-left").innerHTML = `Guesses left: ${guessesMax}`;
+            
+            guessesLeftE1.innerHTML = `Guesses left: ${guessesMax}`;
         }
 
 }
