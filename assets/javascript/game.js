5db796b233e642888cc1512b839d8be71858f3eb
Sandra <sandra.alpha2010@gmail.com>
2020-04-18 00:23:13 -0700

    getelementid added to game js and pseudocoded a couple of pieces of the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+let userGuessesE1 = document.getElementById("user-guesses");
+let guessesLeftE1 = document.getElementById("guesses-left");
+let userWinsE1 = document.getElementById("user-wins");
+let userLossesE1 = document.getElementById("user-losses");
+
+
+let computerChoices = ["q", "z", "a", "e", "d", "c", "t", "g", "b", "u", "j", "m", "o", "l"];
+
+let guessesMax = 6;
+let userPredictions = //
+
+let wins = 0;
+let losses = 0;
+
+//every time the user guesses wrong guessMaxx-- and losses++
+//every time the user guesses correctly win++
